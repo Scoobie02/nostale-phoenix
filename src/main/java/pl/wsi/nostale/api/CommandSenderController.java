48315cca79ccb03610a4c11d531f1bc9b6286cb4
wsi <skubala.wojciech@gmail.com>
2024-07-03 18:16:14 +0200

    add start stop

--- src/main/java/pl/wsi/nostale/api/CommandSenderController.java
+++ src/main/java/pl/wsi/nostale/api/CommandSenderController.java
@@ -1,6 +1,7 @@
 package pl.wsi.nostale.api;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +16,23 @@ class CommandSenderController {
 
 	private final NostaleApi nostaleApi;
 
-	@GetMapping("/walk/{x}/{y}")
+	@GetMapping(value = "/walk/{x}/{y}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Boolean send(@PathVariable("x") Integer x, @PathVariable("y") Integer y) throws IOException {
 		return nostaleApi.playerWalk(x, y);
 	}
 
-	@GetMapping("/querryPlayerInfo")
+	@GetMapping(value = "/querryPlayerInfo", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Boolean querryPlayerInfo() throws IOException {
 		return nostaleApi.queryPlayerInformation();
 	}
 
+	@GetMapping(value = "/stopBot", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Boolean stopBot() throws IOException {
+		return nostaleApi.stopBot();
+	}
+
+	@GetMapping(value = "/startBot", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Boolean startBot() throws IOException {
+		return nostaleApi.startBot();
+	}
 }
