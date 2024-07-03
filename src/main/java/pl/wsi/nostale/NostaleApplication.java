79f18f16b85ed6bf569eaac8c30b0c5505ddba62
wsi <skubala.wojciech@gmail.com>
2024-07-03 16:01:49 +0200

    Init

---
+++ src/main/java/pl/wsi/nostale/NostaleApplication.java
@@ -0,0 +1,13 @@
+package pl.wsi.nostale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NostaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NostaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wsi/nostale/NostaleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wsi.nostale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NostaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
